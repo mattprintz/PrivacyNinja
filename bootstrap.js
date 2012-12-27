501d415530d18355b9f655600dcedd536aecbe1e
Matt Printz <hipokrit@hipokrit.com>
2012-12-27 02:46:50 -0600

    Code refactoring, pref updating. Adding option XULwindow

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,21 @@
-function PrivacyNinja(){}
+function alert(string) {
+    let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
+                     .getService(Components.interfaces.nsIWindowMediator);
+    let win = wm.getMostRecentWindow("navigator:browser");
+    if(win)
+        win.alert(string);
+}
 
+//Components.utils.import("resource://gre/modules/Services.jsm");
+
+function PrivacyNinja(){}
 PrivacyNinja.prototype = {
+    PASSDOM: "chrome://privacyninja", // Password domain
+    
+    _osType: null, // What OS is this running
     _os: null, // Observer service
     _ps: null, // Pref service
     _inPrivateBrowsing: false, // whether we are in private browsing mode
-    _watcher: null, // the watcher object
     process: null, // tunnel process
     
     init: function() {
@@ -13,96 +24,111 @@ PrivacyNinja.prototype = {
                             .getService(Components.interfaces.nsIObserverService);
         this._os.addObserver(this, "private-browsing", false);
         this._os.addObserver(this, "quit-application", false);
-        try {
-            var pbs = Components.classes["@mozilla.org/privatebrowsing;1"]
-                            .getService(Components.interfaces.nsIPrivateBrowsingService);
-            this._inPrivateBrowsing = pbs.privateBrowsingEnabled;
-        } catch(ex) {
-            // ignore exceptions in older versions of Firefox
-        }
+        
+        this._inPrivateBrowsing = Components.classes["@mozilla.org/privatebrowsing;1"].
+                                  getService(Components.interfaces.nsIPrivateBrowsingService).
+                                  privateBrowsingEnabled;
+        
         this._ps = Components.classes["@mozilla.org/preferences-service;1"]
                             .getService(Components.interfaces.nsIPrefService);
         
-        this.setDefaults();
+        var xulRuntime = Components.classes["@mozilla.org/xre/app-info;1"]
+                    .getService(Components.interfaces.nsIXULRuntime);
+        this._osType = xulRuntime.OS;
         
-        var parent = this;
-        this._watcher = {
-            onEnterPrivateBrowsing : function() {
-                let pref = parent._ps.getBranch('');
-                
-                // we have just entered private browsing mode!
-                parent.saveDefaultProxySettings();
-                
-                if(pref.getBoolPref('extensions.privacyninja.use_tunnel')) {
-                    parent.startTunnel();
-                    pref.setCharPref('network.proxy.http', '127.0.0.1');
-                    pref.setIntPref('network.proxy.http_port', pref.getIntPref('extensions.privacyninja.local_port'));
-                    pref.setIntPref('network.proxy.type', 1);
-                }
-                else {
-                    pref.setCharPref('network.proxy.http', pref.getCharPref('extensions.privacyninja.proxy_host'));
-                    pref.setIntPref('network.proxy.http_port', pref.getIntPref('extensions.privacyninja.proxy_port'));
-                    pref.setIntPref('network.proxy.type', 1);
-                }
-                
-            },
-          
-            onExitPrivateBrowsing : function() {
-                // we have just left private browsing mode!
-                let pref = parent._ps.getBranch('');
-                parent.closeTunnel();
-                parent.restoreDefaultProxySettings();
-            }
-        };
+        this.setDefaults();
+    },
+    
+    onEnterPrivateBrowsing : function() {
+        let pref = this._ps.getBranch("");
+        
+        // we have just entered private browsing mode!
+        this.saveDefaultProxySettings();
+        
+        if(pref.getBoolPref("extensions.privacyninja.use_tunnel")) {
+            this.startTunnel();
+            pref.setCharPref("network.proxy.http", "127.0.0.1");
+            pref.setIntPref("network.proxy.http_port", pref.getIntPref("extensions.privacyninja.local_port"));
+            pref.setIntPref("network.proxy.type", 1);
+        }
+        else {
+            pref.setCharPref("network.proxy.http", pref.getCharPref("extensions.privacyninja.proxy_host"));
+            pref.setIntPref("network.proxy.http_port", pref.getIntPref("extensions.privacyninja.proxy_port"));
+            pref.setIntPref("network.proxy.type", 1);
+        }
+        
+    },
+    
+    onExitPrivateBrowsing : function() {
+        // we have just left private browsing mode!
+        this.closeTunnel();
+        this.restoreDefaultProxySettings();
     },
     
     shutdown: function() {
-        this.restoreDefaultProxySettings();
         if(this.process) {
             this.closeTunnel();
         }
+        this.restoreDefaultProxySettings();
         this._os.removeObserver(this, "private-browsing", false);
         this._os.removeObserver(this, "quit-application", false);
     },
     
     setDefaults: function() {
-        let dpref = this._ps.getDefaultBranch('extensions.privacyninja.')
+        Components.utils.import("resource://gre/modules/FileUtils.jsm");
+        
+        let dpref = this._ps.getDefaultBranch("extensions.privacyninja.")
         dpref.setIntPref("proxy_port", 8080);
-        dpref.setIntPref("local_port", 4888);
+        dpref.setIntPref("local_port", 4080);
         dpref.setCharPref("proxy_host", "");
         dpref.setBoolPref("use_tunnel", false);
-        //dpref.setComplexValue("tunnel_executable", Components.interfaces.nsIFile, null);
         
-        //dpref.setCharPref("tunnel_executable", "C:\\Users\\matt\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\gd370pcy.dev\\plink.exe");
-        dpref.setCharPref("tunnel_executable", "C:\\plink.exe");
-        dpref.setCharPref("tunnel_args", "-ssh -N -L $local_port:127.0.0.1:$proxy_port -l $username -pw $password $proxy_host");
+        dpref.setComplexValue("tunnel_keyfile", Components.interfaces.nsIFile, FileUtils.getFile("Home", ["tunnel.key"]));        
         
-        // TODO: Move these to password vault
-        dpref.setCharPref("username", "");
-        dpref.setCharPref("password", "");
+        // Windows
+        if (this._osType == "WINNT") {
+            dpref.setComplexValue("tunnel_executable", Components.interfaces.nsIFile, FileUtils.getFile("Home", ["plink.exe"]));
+            dpref.setCharPref("tunnel_args", "-ssh -N -L $local_port:127.0.0.1:$proxy_port -l $username -i $keyfile $proxy_host");
+        }
         
+        // Linux/Mac
+        else {
+            dpref.setComplexValue("tunnel_executable", Components.interfaces.nsIFile, FileUtils.File("/usr/bin/ssh"));
+            dpref.setCharPref("tunnel_args", "-L $local_port:127.0.0.1:$proxy_port -l $username -i $keyfile $proxy_host");
+        }
+        
+        // Possible feature?
+        //dpref.setBoolPref("enforceChecksumMatch", true);
     },
     
     startTunnel: function() {
-        //Components.utils.import("resource://gre/modules/FileUtils.jsm");
-        // get the "data.txt" file in the profile directory
-        //var file = FileUtils.getFile("ProfD", ["plink.exe"]);
+        try{
+        let pref = this._ps.getBranch("extensions.privacyninja.");
         
-        let pref = this._ps.getBranch('extensions.privacyninja.');
-        
-        var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
-        file.initWithPath(pref.getCharPref("tunnel_executable"));
-        
+        let file = pref.getComplexValue("tunnel_executable", Components.interfaces.nsIFile);
         
         this.process = Components.classes["@mozilla.org/process/util;1"]
                     .createInstance(Components.interfaces.nsIProcess);
         
+        let username = "",
+            password = "";
+        let login = this.getPassword();
+        if(login) {
+            username = login.user;
+            password = login.pass;
+        }
+        else {
+            alert("Need username/password for tunnel");
+            return;
+        }
+        
         var arg_map = {
-            '$local_port': pref.getIntPref('local_port'),
-            '$proxy_port': pref.getIntPref('proxy_port'),
-            '$proxy_host': pref.getCharPref('proxy_host'),
-            '$username': pref.getCharPref('username'),
-            '$password': pref.getCharPref('password'),
+            "$local_port": pref.getIntPref("local_port"),
+            "$proxy_port": pref.getIntPref("proxy_port"),
+            "$proxy_host": pref.getCharPref("proxy_host"),
+            "$username": username,
+            "$password": password,
+            "$keyfile": pref.getComplexValue("tunnel_keyfile", Components.interfaces.nsIFile).path,
         },
         
         args = pref.getCharPref("tunnel_args").split(" ");
@@ -115,90 +141,93 @@ PrivacyNinja.prototype = {
         this.process.init(file);
         this.process.run(false, args, args.length);
         
-        //this.refocus();
+        }catch(e) {
+            alert(e);
+        }
     },
     
     closeTunnel: function() {
         this.process.kill();
+        this.process = null;
     },
-    
-    //refocus: function() {
-    //    let wm = Components.classes["@mozilla.org/appshell/window-mediator;1"]
-    //                     .getService(Components.interfaces.nsIWindowMediator);
-    //    let win = wm.getMostRecentWindow("navigator:browser");
-    //    //let domWindowUtils = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-    //    //             .getInterface(Components.interfaces.nsIDOMWindowUtils);
-    //    //win.setTimeout(function() {domWindowUtils.focus(win);}, 300);
-    //    //win.setTimeout(function() {win.blur(); win.focus();}, 300);
-    //},
     
     saveDefaultProxySettings: function() {
         if(this.hasSavedProxySettings()) {
             return;
         }
-        let pref = this._ps.getBranch('');
-        pref.setIntPref('extensions.privacyninja.defaults.proxy_type', pref.getIntPref('network.proxy.type'));
-        pref.setCharPref('extensions.privacyninja.defaults.proxy_http', pref.getCharPref('network.proxy.http'));
-        pref.setIntPref('extensions.privacyninja.defaults.proxy_http_port', pref.getIntPref('network.proxy.http_port'));
+        let pref = this._ps.getBranch("");
+        pref.setIntPref("extensions.privacyninja.defaults.proxy_type", pref.getIntPref("network.proxy.type"));
+        pref.setCharPref("extensions.privacyninja.defaults.proxy_http", pref.getCharPref("network.proxy.http"));
+        pref.setIntPref("extensions.privacyninja.defaults.proxy_http_port", pref.getIntPref("network.proxy.http_port"));
     },
     
     restoreDefaultProxySettings: function() {
-        let pref = this._ps.getBranch('');
-        if(pref.prefHasUserValue('extensions.privacyninja.defaults.proxy_type'))
-            pref.setIntPref('network.proxy.type', pref.getIntPref('extensions.privacyninja.defaults.proxy_type'));
-        if(pref.prefHasUserValue('extensions.privacyninja.defaults.proxy_http'))
-            pref.setCharPref('network.proxy.http', pref.getCharPref('extensions.privacyninja.defaults.proxy_http'));
-        if(pref.prefHasUserValue('extensions.privacyninja.defaults.proxy_http_port'))
-            pref.setIntPref('network.proxy.http_port', pref.getIntPref('extensions.privacyninja.defaults.proxy_http_port'));
-        pref.clearUserPref('extensions.privacyninja.defaults.proxy_type');
-        pref.clearUserPref('extensions.privacyninja.defaults.proxy_http');
-        pref.clearUserPref('extensions.privacyninja.defaults.proxy_http_port');
+        let pref = this._ps.getBranch("");
+        if(pref.prefHasUserValue("extensions.privacyninja.defaults.proxy_type"))
+            pref.setIntPref("network.proxy.type", pref.getIntPref("extensions.privacyninja.defaults.proxy_type"));
+        if(pref.prefHasUserValue("extensions.privacyninja.defaults.proxy_http"))
+            pref.setCharPref("network.proxy.http", pref.getCharPref("extensions.privacyninja.defaults.proxy_http"));
+        if(pref.prefHasUserValue("extensions.privacyninja.defaults.proxy_http_port"))
+            pref.setIntPref("network.proxy.http_port", pref.getIntPref("extensions.privacyninja.defaults.proxy_http_port"));
+        pref.clearUserPref("extensions.privacyninja.defaults.proxy_type");
+        pref.clearUserPref("extensions.privacyninja.defaults.proxy_http");
+        pref.clearUserPref("extensions.privacyninja.defaults.proxy_http_port");
     },
     
     hasSavedProxySettings: function() {
-        let pref = this._ps.getBranch('extensions.privacyninja.defaults.');
-        return pref.prefHasUserValue('proxy_type') ||
-               pref.prefHasUserValue('proxy_http') ||
-               pref.prefHasUserValue('proxy_http_port');
+        let pref = this._ps.getBranch("extensions.privacyninja.defaults.");
+        return pref.prefHasUserValue("proxy_type") ||
+               pref.prefHasUserValue("proxy_http") ||
+               pref.prefHasUserValue("proxy_http_port");
     },
     
+    getPassword: function() {
+        let loginManager = Components.classes["@mozilla.org/login-manager;1"]
+                           .getService(Components.interfaces.nsILoginManager);
+        let logins = loginManager.findLogins({}, this.PASSDOM, null, "ssh_tunnel");
+        if (logins.length > 0) {
+            return {
+                "user": logins[0].username,
+                "pass": logins[0].password
+            };
+        }
+        else {
+            return null;
+        }
+    },
+    
+    setPassword: function(username, password) {
+        let loginManager = Components.classes["@mozilla.org/login-manager;1"]
+                           .getService(Components.interfaces.nsILoginManager);
+        let nsLoginInfo = new Components.Constructor("@mozilla.org/login-manager/loginInfo;1", Components.interfaces.nsILoginInfo, "init");
+        let loginInfo = new nsLoginInfo(this.PASSDOM, null, "ssh_tunnel", username, password, "", "");
+        loginManager.addLogin(loginInfo);
+    },
     
     observe: function (aSubject, aTopic, aData) {
         if (aTopic == "private-browsing") {
             if (aData == "enter") {
                 this._inPrivateBrowsing = true;
-                if (this.watcher &&
-                        "onEnterPrivateBrowsing" in this._watcher) {
-                    this.watcher.onEnterPrivateBrowsing();
-                }
+                this.onEnterPrivateBrowsing();
             } else if (aData == "exit") {
                 this._inPrivateBrowsing = false;
-                if (this.watcher &&
-                        "onExitPrivateBrowsing" in this._watcher) {
-                    this.watcher.onExitPrivateBrowsing();
-                }
+                this.onExitPrivateBrowsing();
             }
-        } else if (aTopic == "quit-application") {
-            this.watcher.onExitPrivateBrowsing();
-            this._os.removeObserver(this, "quit-application");
-            this._os.removeObserver(this, "private-browsing");
+        }
+        else if (aTopic == "quit-application") {
+            this.shutdown();
+        }
+        else if (aTopic == "domwindowopened") {
+            // TODO: Add way to open optiosn from dom windows
         }
     },
   
     get inPrivateBrowsing() {
         return this._inPrivateBrowsing;
     },
-    
-    get watcher() {
-        return this._watcher;
-    },
-    
-    set watcher(val) {
-        this._watcher = val;
-    }
 }
 
-
+// Define global PrivacyNinja instance
 var privacyninja = new PrivacyNinja();
 
 function startup(data, reason) {
@@ -207,6 +236,7 @@ function startup(data, reason) {
         privacyninja.restoreDefaultProxySettings();
     }
 }
+
 function shutdown(data, reason) {
     if(privacyninja.hasSavedProxySettings()) {
         privacyninja.restoreDefaultProxySettings();
@@ -214,6 +244,8 @@ function shutdown(data, reason) {
     privacyninja.shutdown();
 }
 
+// Unused
 function install(data, reason) {}
 
+// Unused
 function uninstall(data, reason) {}
